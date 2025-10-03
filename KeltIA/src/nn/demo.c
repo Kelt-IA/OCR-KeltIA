d90c692143779f451f2710ca73cf57ac4e55142e
Ethan Rouimi <contact@ethan-rouimi.com>
2025-10-03 02:16:12 +0200

    comments

--- KeltIA/src/nn/demo.c
+++ KeltIA/src/nn/demo.c
@@ -7,14 +7,18 @@ NeuronalNetwork *xor_nn()
     size_t num_neuron_l[2] = {2, 1};
     NeuronalNetwork *nn = create_nn(2, 2, num_neuron_l);
 
-    // hand crafted weights and
-    double first_w[4] = {1, 1, 1, 1};
-    double first_b[2] = {-0.5, -1.5};
-    double second_w[2] = {1, -1};
-    double second_b[1] = {-0.5};
+    // hand crafted weights and biases
+    double first_w[4] = {1, 1, 1, 1};  // 2 neurons, 2 inputs each -> 4 weights
+    double first_b[2] = {-0.5, -1.5};  // 2 neurons, 2 biases
 
+    double second_w[2] = {1, -1};  // 1 neuron,  2 inputs -> 2 weights
+    double second_b[1] = {-0.5};   // 1 neuron,  1 bias
+
+    // layer 1 (hidden layer)
     load_weights(&nn->layers[0], first_w);
     load_biases(&nn->layers[0], first_b);
+
+    // layer 2 (exit)
     load_weights(&nn->layers[1], second_w);
     load_biases(&nn->layers[1], second_b);
 
