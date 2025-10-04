f4648813d6b040ea42ce4805eb22e6eaf80598aa
Ethan Rouimi <contact@ethan-rouimi.com>
2025-10-05 01:35:19 +0200

    add test for xor nn

--- KeltIA/tests/main.c
+++ KeltIA/tests/main.c
@@ -2,25 +2,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define COLOR_GREEN "\x1b[32m"
-#define COLOR_RED "\x1b[31m"
-#define COLOR_RESET "\x1b[0m"
-
-void print_test_result(const char *test_name, int passed)
-{
-    if (passed == 1)
-    {
-        printf("%s[PASS]%s %s\n", COLOR_GREEN, COLOR_RESET, test_name);
-        return;
-    }
-
-    printf("%s[FAIL]%s %s\n", COLOR_RED, COLOR_RESET, test_name);
-}
-
 int main()
 {
+    print_centered_header("Save and Load NNs");
     print_test_result("write nn", test_write_nn());
     print_test_result("load nn", test_load_nn());
-    print_test_result("write nn, load", test_write_and_load());
+    print_test_result("write nn, load", test_write_and_load(0));
+
+    print_centered_header("XOR NN for demo");
+    print_test_result("test XOR nn", test_xor_nn());
     return 0;
 }
