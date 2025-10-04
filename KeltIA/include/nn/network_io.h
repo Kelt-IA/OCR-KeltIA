b86a464f60f00fd4f32057659ff5ccd9d5161af1
Ethan Rouimi <contact@ethan-rouimi.com>
2025-10-05 01:35:04 +0200

    fix warning

--- KeltIA/include/nn/network_io.h
+++ KeltIA/include/nn/network_io.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#include "errors.h"
 #include "network.h"
 
 // used for verifying the format of the file
 #define MAGIC "NNET"
 #define MAGIC_SIZE 4
 
-int save_nn(const char *path, const NeuronalNetwork *nn);
-int load_nn(const char *path, NeuronalNetwork *out_nn);
+ErrorCode save_nn(const char *path, const NeuronalNetwork *nn);
+ErrorCode load_nn(const char *path, NeuronalNetwork *out_nn);
 
 // structure of custom file:
 // (binary format)
