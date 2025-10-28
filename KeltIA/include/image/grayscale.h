a144aca1aade71b5d111a15ad6da3822fc68736b
TirouwwW <184528283+TirouwwW@users.noreply.github.com>
2025-10-28 22:05:47 +0100

    resoud_change

--- KeltIA/include/image/grayscale.h
+++ KeltIA/include/image/grayscale.h
@@ -1,27 +1,6 @@
 #pragma once
 #include <MagickWand/MagickWand.h>
 
-<<<<<<< HEAD
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-    int grayscale_threshold_file(
-        const char *in_path,
-        const char *out_path,
-        double threshold
-    );
-    MagickBooleanType
-    grayscale_threshold_wand(MagickWand *wand, double threshold);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
-=======
 static double compute_average_gray(MagickWand *wand);
-MagickBooleanType binarize_image(const char *input_path,
-                                 const char *output_path);
->>>>>>> a912520 (1)
+MagickBooleanType
+binarize_image(const char *input_path, const char *output_path);
