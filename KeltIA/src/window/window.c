53ffa0c4c4582791bea84e3a7acb136b0c86ffaa
biolix <68819763+biolix06@users.noreply.github.com>
2025-12-03 12:52:16 +0100

    Rotation CB untested

--- KeltIA/src/window/window.c
+++ KeltIA/src/window/window.c
@@ -49,7 +49,11 @@ void init_window(int *argc, char ***argv)
     // Destroy stop the program when main window is destroyed
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+<<<<<<< HEAD
     callback_init(builder, window);
+=======
+    callback_init(builder);
+>>>>>>> 8ce621d (Rotation CB untested)
 
     gtk_widget_show_all(window);
     gtk_widget_realize(window);
