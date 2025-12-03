8ce621dfe13c0268b54da5c0653859dfb1156ba0
biolix <68819763+biolix06@users.noreply.github.com>
2025-12-03 12:52:16 +0100

    Rotation CB untested

--- KeltIA/src/window/callback.c
+++ KeltIA/src/window/callback.c
@@ -1,24 +1,37 @@
-void callback_init()
-{
+#include "../../include/window/callback.h"
 
+GtkBuilder *builder = NULL;
+GtkWidget *rotation_check = NULL;
+GtkWidget *rotation_box = NULL;
+
+void callback_init(GtkBuilder *b)
+{
+    builder = b;
+    if (!(rotation_box =
+              GTK_WIDGET(gtk_builder_get_object(builder, "rotation_gbox"))))
+        errx(EXIT_FAILURE, "Could not find rotation box");
+
+    if (!(rotation_check =
+              GTK_WIDGET(gtk_builder_get_object(builder, "auto_rotation_btn"))))
+        errx(EXIT_FAILURE, "Could not find rotation checkbox");
+
+    g_signal_connect(
+        rotation_check, "clicked", G_CALLBACK(on_autorotate_check), NULL
+    );
 }
 
-void open_image()
+void on_autorotate_check(GtkToggleButton *toggle_button, gpointer user_data)
 {
+    gboolean active = gtk_toggle_button_get_active(toggle_button);
 
+    if (active) { gtk_widget_set_sensitive(rotation_box, FALSE); }
+    else { gtk_widget_set_sensitive(rotation_box, TRUE); }
 }
 
-void open_nn()
-{
+void on_open_image() {}
 
-}
+void on_open_nn() {}
 
-void save()
-{
+void on_save() {}
 
-}
-
-void solve()
-{
-
-}
+void on_solve() {}
