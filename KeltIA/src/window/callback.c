f44382f0283cfa8427062e444765be7fecdb61e3
biolix <68819763+biolix06@users.noreply.github.com>
2025-12-13 00:27:16 +0100

    linked callback

--- KeltIA/src/window/callback.c
+++ KeltIA/src/window/callback.c
@@ -1,12 +1,29 @@
 #include "../../include/window/callback.h"
+#include "../../include/window/utils.h"
+#include "gtk/gtk.h"
 
 GtkBuilder *builder = NULL;
+GtkWidget *window = NULL;
+
 GtkWidget *rotation_check = NULL;
 GtkWidget *rotation_box = NULL;
+
+GtkWidget *viewport = NULL;
+GtkWidget *load_img_btn = NULL;
+char *img = NULL;
+
+GtkWidget *load_nn_btn = NULL;
+char *nn = NULL;
+
+GtkWidget *solve_btn = NULL;
 
 void callback_init(GtkBuilder *b)
 {
     builder = b;
+
+    if (!(window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"))))
+        errx(EXIT_FAILURE, "Could not find the main window");
+
     if (!(rotation_box =
               GTK_WIDGET(gtk_builder_get_object(builder, "rotation_gbox"))))
         errx(EXIT_FAILURE, "Could not find rotation box");
@@ -18,6 +35,27 @@ void callback_init(GtkBuilder *b)
     g_signal_connect(
         rotation_check, "clicked", G_CALLBACK(on_autorotate_check), NULL
     );
+
+    if (!(viewport =
+              GTK_WIDGET(gtk_builder_get_object(builder, "viewport_img"))))
+        errx(EXIT_FAILURE, "Could not find viewport");
+
+    if (!(load_img_btn =
+              GTK_WIDGET(gtk_builder_get_object(builder, "load_image_btn"))))
+        errx(EXIT_FAILURE, "Could not find load img btn");
+
+    g_signal_connect(load_img_btn, "clicked", G_CALLBACK(on_open_image), NULL);
+
+    if (!(load_nn_btn =
+              GTK_WIDGET(gtk_builder_get_object(builder, "load_model_btn"))))
+        errx(EXIT_FAILURE, "Could not find load model btn");
+
+    g_signal_connect(load_nn_btn, "clicked", G_CALLBACK(on_open_nn), NULL);
+
+    if (!(solve_btn = GTK_WIDGET(gtk_builder_get_object(builder, "solve_btn"))))
+        errx(EXIT_FAILURE, "Could not find solve btn");
+
+    g_signal_connect(solve_btn, "clicked", G_CALLBACK(on_solve), NULL);
 }
 
 void on_autorotate_check(GtkToggleButton *toggle_button, gpointer user_data)
@@ -27,19 +65,111 @@ void on_autorotate_check(GtkToggleButton *toggle_button, gpointer user_data)
     gboolean active = gtk_toggle_button_get_active(toggle_button);
 
     if (active) { gtk_widget_set_sensitive(rotation_box, FALSE); }
-    else { gtk_widget_set_sensitive(rotation_box, TRUE); }
+    else
+    {
+        gtk_widget_set_sensitive(rotation_box, TRUE);
+    }
 }
 
-void on_open_image() {}
+void on_open_image(GtkButton *btn, gpointer user_data)
+{
+    (void)btn;
+    (void)user_data;
 
-void on_open_nn() {}
+    GtkWidget *parent = window;
+    GtkWidget *dialog;
+    gint res;
+
+    dialog = gtk_file_chooser_dialog_new(
+        "Open image", GTK_WINDOW(parent), GTK_FILE_CHOOSER_ACTION_OPEN,
+        "_Cancel", GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL
+    );
+
+    res = gtk_dialog_run(GTK_DIALOG(dialog));
+
+    if (res == GTK_RESPONSE_ACCEPT)
+    {
+        char *filename;
+        GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+
+        GtkFileFilter *filter = gtk_file_filter_new();
+        gtk_file_filter_set_name(filter, "Image files");
+
+        gtk_file_filter_add_pattern(filter, "*.png");
+        gtk_file_filter_add_pattern(filter, "*.jpg");
+        gtk_file_filter_add_pattern(filter, "*.jpeg");
+        gtk_file_filter_add_pattern(filter, "*.bmp");
+
+        gtk_file_chooser_add_filter(chooser, filter);
+        gtk_file_chooser_set_filter(chooser, filter);
+
+        filename = gtk_file_chooser_get_filename(chooser);
+
+        if (!is_supported_by_magickwand(filename))
+        {
+            g_free(filename);
+            return;
+        }
+
+        if (img)
+        {
+            remove(img);
+            free(img);
+        }
+
+        img = copy_to_temp_file_path((const char *)img);
+
+        g_free(filename);
+    }
+
+    gtk_widget_destroy(dialog);
+}
+
+void on_open_nn(GtkButton *btn, gpointer user_data)
+{
+    (void)btn;
+    (void)user_data;
+
+    GtkWidget *parent = window;
+    GtkWidget *dialog;
+    gint res;
+
+    dialog = gtk_file_chooser_dialog_new(
+        "Open neural network", GTK_WINDOW(parent), GTK_FILE_CHOOSER_ACTION_OPEN,
+        "_Cancel", GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL
+    );
+
+    res = gtk_dialog_run(GTK_DIALOG(dialog));
+
+    if (res == GTK_RESPONSE_ACCEPT)
+    {
+        char *filename;
+        GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+
+        GtkFileFilter *filter = gtk_file_filter_new();
+        gtk_file_filter_set_name(filter, "Neural network files");
+
+        gtk_file_filter_add_pattern(filter, "*.nn");
+
+        gtk_file_chooser_add_filter(chooser, filter);
+        gtk_file_chooser_set_filter(chooser, filter);
+
+        filename = gtk_file_chooser_get_filename(chooser);
+
+        if (nn) { g_free(nn); }
+
+        nn = filename;
+    }
+
+    gtk_widget_destroy(dialog);
+}
 
 void on_save() {}
 
-void on_solve() {}
+void on_solve(GtkButton *btn, gpointer user_data) {}
 
-void on_open_training(GtkToggleButton *toggle_button, gpointer user_data)
+void on_open_training(GtkButton *btn, gpointer user_data)
 {
-    (void)toggle_button;
+    (void)btn;
     (void)user_data;
 }
---
+++ KeltIA/src/window/utils.c
@@ -0,0 +1,84 @@
+#include "../../include/window/utils.h"
+
+MagickBooleanType is_supported_by_magickwand(char *path)
+{
+    MagickWand *wand;
+    MagickBooleanType status;
+
+    MagickWandGenesis();
+
+    wand = NewMagickWand();
+
+    status = MagickPingImage(wand, path);
+
+    if (wand) DestroyMagickWand(wand);
+
+    MagickWandTerminus();
+    return status;
+}
+
+char *copy_to_temp_file_path(const char *source_path)
+{
+    FILE *source_fp, *temp_fp;
+    char temp_name[L_tmpnam];
+    char *result_path = NULL;
+    char buffer[BUFSIZ];
+    size_t bytes_read;
+
+    if (tmpnam(temp_name) == NULL)
+    {
+        perror("tmpnam failed");
+        return NULL;
+    }
+
+    source_fp = fopen(source_path, "rb");
+    if (source_fp == NULL)
+    {
+        perror("Failed to open source file");
+        return NULL;
+    }
+
+    // Create temp file in binary write mode
+    temp_fp = fopen(temp_name, "wb");
+    if (temp_fp == NULL)
+    {
+        perror("Failed to create temp file");
+        fclose(source_fp);
+        return NULL;
+    }
+
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), source_fp)) > 0)
+    {
+        if (fwrite(buffer, 1, bytes_read, temp_fp) != bytes_read)
+        {
+            perror("Write error");
+            fclose(source_fp);
+            fclose(temp_fp);
+            remove(temp_name);
+            return NULL;
+        }
+    }
+
+    if (ferror(source_fp))
+    {
+        perror("Read error");
+        fclose(source_fp);
+        fclose(temp_fp);
+        remove(temp_name);
+        return NULL;
+    }
+
+    fclose(source_fp);
+    fclose(temp_fp);
+
+    result_path = strdup(temp_name);
+    if (result_path == NULL)
+    {
+        perror("strdup failed");
+        remove(temp_name);
+        return NULL;
+    }
+
+    // printf("File copied to temp: %s\n", result_path);
+    return result_path;
+}
