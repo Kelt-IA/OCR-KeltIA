1123bc1411c1f42875b6fd9086f3b73654e58c48
biolix <68819763+biolix06@users.noreply.github.com>
2025-12-13 00:27:16 +0100

    linked callback

--- KeltIA/include/window/callback.h
+++ KeltIA/include/window/callback.h
@@ -5,3 +5,8 @@
 
 void callback_init(GtkBuilder *b);
 void on_autorotate_check(GtkToggleButton *toggle_button, gpointer user_data);
+void on_open_image(GtkButton *btn, gpointer user_data);
+void on_open_nn(GtkButton *btn, gpointer user_data);
+// void on_save(GtkButton *btn, gpointer user_data);
+void on_solve(GtkButton *btn, gpointer user_data);
+void on_open_training(GtkButton *btn, gpointer user_data);
