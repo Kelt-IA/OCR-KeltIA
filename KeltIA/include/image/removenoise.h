b08c25cf85492f9935bfc9457007ef97d0ebd80c
Ethan Rouimi <contact@ethan-rouimi.com>
2025-12-13 16:18:27 +0100

    comment unsused functions

--- KeltIA/include/image/removenoise.h
+++ KeltIA/include/image/removenoise.h
@@ -7,5 +7,5 @@
 MagickWand *remove_noise(MagickWand *wand);
 MagickWand *read_image(const char *path);
 int write_image(MagickWand *wand, const char *path);
-static double compute_gray_stddev(MagickWand *wand);
-static double auto_radius(double stddev);
+// static double compute_gray_stddev(MagickWand *wand);
+// static double auto_radius(double stddev);
