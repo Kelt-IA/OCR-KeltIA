b28b665500b17f4cbe882c497a3128ed55442a41
biolix <68819763+biolix06@users.noreply.github.com>
2025-12-13 17:11:06 +0100

    Added show result func

--- KeltIA/include/solver/solver.h
+++ KeltIA/include/solver/solver.h
@@ -1,3 +1,10 @@
 #pragma once
+#include <stdlib.h>
 
-void solver(char *grid, int height, int width, const char *word, int word_len);
+typedef struct
+{
+    int x1, y1, x2, y2;
+} WordPos;
+
+WordPos *
+solver(char *grid, int height, int width, const char *word, int word_len);
